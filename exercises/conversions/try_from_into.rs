9a36794d5961fe482cb736b574d91b633384c6d8
btqx <2574633154@qq.com>
2024-04-26 14:44:25 +0800

    96

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -1,15 +1,12 @@
 // try_from_into.rs
-//
-// TryFrom is a simple and safe type conversion that may fail in a controlled
-// way under some circumstances. Basically, this is the same as From. The main
-// difference is that this should return a Result type instead of the target
-// type itself. You can read more about it at
-// https://doc.rust-lang.org/std/convert/trait.TryFrom.html
-//
-// Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for
-// a hint.
+// TryFrom is a simple and safe type conversion that may fail in a controlled way under some circumstances.
+// Basically, this is the same as From. The main difference is that this should return a Result type
+// instead of the target type itself.
+// You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
+// Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for a hint.
 
 use std::convert::{TryFrom, TryInto};
+use std::num::TryFromIntError;
 
 #[derive(Debug, PartialEq)]
 struct Color {
@@ -27,53 +24,141 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
-// Your task is to complete this implementation and return an Ok result of inner
-// type Color. You need to create an implementation for a tuple of three
-// integers, an array of three integers, and a slice of integers.
+// Your task is to complete this implementation
+// and return an Ok result of inner type Color.
+// You need to create an implementation for a tuple of three integers,
+// an array of three integers, and a slice of integers.
 //
-// Note that the implementation for tuple and array will be checked at compile
-// time, but the slice implementation needs to check the slice length! Also note
-// that correct RGB color values must be integers in the 0..=255 range.
+// Note that the implementation for tuple and array will be checked at compile time,
+// but the slice implementation needs to check the slice length!
+// Also note that correct RGB color values must be integers in the 0..=255 range.
 
-// Tuple implementation
-impl TryFrom<(i16, i16, i16)> for Color {
+impl TryFrom<(i16, i16, i16)> for Color 
+{
     type Error = IntoColorError;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> 
+    {
+        //todo!()
+        //tuple_range(tuple);
+
+        match tuple_range(tuple)
+        {
+            Ok(v) => 
+            {
+                return Ok( Color{ red: v[0], green: v[1], blue: v[2]} );
+            }
+
+            Err(_e) => { return Err(IntoColorError::IntConversion); }
+        }
     }
 }
 
 // Array implementation
-impl TryFrom<[i16; 3]> for Color {
+impl TryFrom<[i16; 3]> for Color 
+{
     type Error = IntoColorError;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> 
+    {
+        match array_range(arr)
+        {
+            Ok(v) => 
+            {
+                return Ok( Color{ red: v[0], green: v[1], blue: v[2]} );
+            }
+
+            Err(_e) => { return Err(IntoColorError::IntConversion); }
+        }
     }
 }
 
-// Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> 
+    {
+        //todo!();
+
+        if slice.len() != 3
+        {
+            return Err(Self::Error::BadLen);
+        }
+
+        match slice_range(slice)
+        {
+            Ok(v) => 
+            {
+                return Ok( Color{ red: v[0], green: v[1], blue: v[2]} );
+            }
+
+            Err(_e) => { return Err(IntoColorError::IntConversion); }
+        }
     }
 }
 
-fn main() {
-    // Use the `try_from` function
-    let c1 = Color::try_from((183, 65, 14));
-    println!("{:?}", c1);
+fn main()
+{
+    let a = [1, 2, 3, 4];
+    println!("{}", a[0..2].len());
 
-    // Since TryFrom is implemented for Color, we should be able to use TryInto
-    let c2: Result<Color, _> = [183, 65, 14].try_into();
-    println!("{:?}", c2);
+    
+}
 
-    let v = vec![183, 65, 14];
-    // With slice we should use `try_from` function
-    let c3 = Color::try_from(&v[..]);
-    println!("{:?}", c3);
-    // or take slice within round brackets and use TryInto
-    let c4: Result<Color, _> = (&v[..]).try_into();
-    println!("{:?}", c4);
+fn tuple_range(t: (i16, i16, i16)) -> Result<Vec<u8>, TryFromIntError>
+{
+
+    let temp_vec = vec![t.0, t.1, t.2];
+    let mut temp_vec2 = vec![];
+
+    for num in temp_vec.into_iter()
+    {
+        match u8::try_from(num)
+        {
+            Ok(n) => temp_vec2.push(n),
+            Err(e)    => return Err(e)
+        };
+    }
+    Ok(temp_vec2)
+}
+
+fn array_range(a: [i16; 3]) -> Result<[u8; 3], TryFromIntError>
+{
+
+    let mut c = [0, 0, 0];
+    let mut index: usize = 0;
+
+    for n in a
+    {
+         
+        match u8::try_from(n)
+        {
+            Ok(n) =>  { c[index] = n; },
+            Err(e) => return Err(e),
+        }
+        index += 1;
+    }
+
+    Ok(c)
+
+}
+
+fn slice_range(slice: &[i16]) -> Result<[u8; 3], TryFromIntError>
+{
+    let mut c = [0, 0, 0];
+    let mut index: usize = 0;
+
+    for n in slice
+    {
+         
+        match u8::try_from(*n)
+        {
+            Ok(n) =>  { c[index] = n; },
+            Err(e) => return Err(e),
+        }
+        index += 1;
+    }
+
+    Ok(c)
 }
 
 #[cfg(test)]
@@ -114,6 +199,7 @@ mod tests {
             }
         );
     }
+
     #[test]
     fn test_array_out_of_range_positive() {
         let c: Result<Color, _> = [1000, 10000, 256].try_into();
@@ -142,6 +228,7 @@ mod tests {
             }
         );
     }
+
     #[test]
     fn test_slice_out_of_range_positive() {
         let arr = [10000, 256, 1000];
@@ -190,4 +277,5 @@ mod tests {
         let v = vec![0, 0];
         assert_eq!(Color::try_from(&v[..]), Err(IntoColorError::BadLen));
     }
+
 }
--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,16 +6,18 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+// Step 1.
+// Complete the `capitalize_first` function.
+// "hello" -> "Hello"
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
-        None => String::new(),
-        Some(first) => ???,
+        None => String::new(), // 返回空： ""
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -24,7 +26,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // vec![]
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -32,7 +35,8 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // String::new()
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 #[cfg(test)]
