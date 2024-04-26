f38701b79a68c3e86383ccd871e00d9baa949cc5
btqx <2574633154@qq.com>
2024-04-26 13:35:17 +0800

    57

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -32,7 +32,7 @@ mod tests {
     #[test]
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the
-        // Option?
+        // Option?11
         let icecreams = maybe_icecream(12);
         assert_eq!(icecreams, 5);
     }
